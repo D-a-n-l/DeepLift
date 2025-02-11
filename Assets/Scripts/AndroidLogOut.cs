c3bb2d3ed4df7d8f00e29b894475f929f6efca4b
Daniil Oleynikov <danilachka225@gmail.com>
2025-02-11 13:35:20 +0300

    Added system LogOut from Acc

---
+++ Assets/Scripts/AndroidLogOut.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AndroidLogOut : MonoBehaviour
+{
+    [SerializeField]
+    private Button button;
+
+    private void Start()
+    {
+        button.onClick.AddListener(AndroidSignIn.SignOut);
+    }
+}
